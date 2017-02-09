15615a53b9a161016efd8e6655c9abc3edf824a6
Christopher <ccammo@gmail.com>
2017-02-09 13:31:50 -0500

    Update NumbersDemo.java

--- NumbersDemo.java
+++ NumbersDemo.java
@@ -2,16 +2,30 @@ import javax.swing.JOptionPane;
 public class NumbersDemo
 
 {
-	public static void main(String Args[])
+	public static void main(String[] Args)
 	{
-		final int q = 4; 
-		//Number of quarts per gallon
-		final double quart;
-		String quartString;
-            quartString = JOptionPane.showInputDialog("Enter number of quarts to convert to Gallon(s))", "Quarts");
-				
-			quart = Double.parseDouble(quartString);
-				JOptionPane.showMessageDialog(null,"You have " + quart / q + " gallons.");
-		
+		int a = 5;
+		int b = 10;
+		displayTwiceTheNumber(a, b);
+		displayNumberPlusFive(a, b);
+		displayNumberSquared(a, b);
+	}
+	public static void displayTwiceTheNumber(int a, int b)
+	{
+		System.out.println("Integer a is " + a + " and integer b is " + b);
+		System.out.println("Display two times the integer");
+		System.out.println(a + " * 2 = " + (a * 2) + " and " + b + " * 2 = " + (b * 2));
+	}
+	public static void displayNumberPlusFive(int a, int b)
+	{
+		System.out.println("Display integer plus five");
+		System.out.println("Integer a = " + a + " + " + 5 + " = " + (a + 5));
+		System.out.println("Integer b = " + b + " = " + 5 + " = " + (b + 5));
+	}
+	public static void displayNumberSquared(int a, int b)
+	{
+		System.out.println("Display the integer squared");
+		System.out.println("Integer a = " + (a * a));
+		System.out.println("Integer b = " + (b * b));
 	}
 }
